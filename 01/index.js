59e9fc5967c28b94fbfb6cbddf43e78ff3d59536
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-05-02 13:24:51 +0200

    first

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const Paragraph = () => {
+    return React.createElement(
+        'p',
+        {
+            id: 'first-paragraph',
+            className: 'paragraph'
+        },
+        'lorem ipsum dolor sit amet'
+    );
+};
+const appContainer = document.querySelector('#app');
+
+ReactDOM.render(React.createElement(Paragraph), appContainer);
